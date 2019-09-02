967bbccf240d39810e13c9d4f4f9323bef603267
mxmckee <41008554+mxmckee@users.noreply.github.com>
2019-09-01 23:13:52 -0500

    Adding working solution created on another branch.

---
+++ app/src/main/java/com/ualr/helloworld/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ualr.helloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView userMsgTV;
+    private EditText userInputET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userMsgTV = findViewById(R.id.userMsgTV);
+        userInputET = findViewById(R.id.userInputET);
+    }
+
+    public void showTextMessage(View view) {
+        if (!userInputET.getText().toString().isEmpty()) {
+            userMsgTV.setText(userInputET.getText().toString());
+        }
+    }
+
+    public void cleanTextField(View view) {
+        userInputET.setText("");
+    }
+}
